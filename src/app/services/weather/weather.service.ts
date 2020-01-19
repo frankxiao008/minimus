2fdd0754d094164727d88b5ef8fff48ce8cbbc6f
Saihong Xiao <sxiao15@myseneca.ca>
2020-01-19 16:46:11 -0500

    add card,home, detail componentds for this

---
+++ src/app/services/weather/weather.service.ts
@@ -0,0 +1,10 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular'
+
+@Injectable({
+  providedIn: 'root'
+})
+export class WeatherService {
+
+  constructor() { }
+}
